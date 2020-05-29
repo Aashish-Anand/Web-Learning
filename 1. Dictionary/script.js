7da7fe2d820cdde6bcbce5856c1a48be0c54d357
Aashish-Anand <aashishanand1997@gmail.com>
2020-05-29 21:52:21 +0530

    added new Dictionary Project

--- 1. Dictionary/script.js
+++ 1. Dictionary/script.js
@@ -1,15 +1,25 @@
 let input = document.querySelector("#input");
 let searchBtn = document.querySelector("#search");
+let notFound = document.querySelector(".not_found ");
+let defBox = document.querySelector(".def");
+let audioBox = document.querySelector(".audio");
+let loading = document.querySelector(".loading");
+let apiKey = ''; // your key should be there
 
 searchBtn.addEventListener('click', function(e) {
-    e.preventDefault(); // to stop any default behaiour to happen
-    alert(1);
 
+    e.preventDefault(); // to stop any default behaiour to happen
+    //alert(1);
+
+    //clear data 
+    audioBox.innerHTML = '';
+
+    notFound.innerText = '';
+
+    defBox.innerText = '';
 
     // Get input data 
     let word = input.value;
-
-
 
     // call API get data
 
@@ -21,6 +31,62 @@ searchBtn.addEventListener('click', function(e) {
     getData(word);
 })
 
-funtion getData(word)[
+async function getData(word) {
+
+    loading.style.display = 'block';
+
     //Ajax Call 
-]
+    const response = await fetch(`https://www.dictionaryapi.com/api/v3/references/learners/json/${word}?key=${apiKey}`);
+
+    const data = await response.json();
+
+    // if data is empty
+    if (!data.length) {
+        loading.style.display = 'none';
+        notFound.innerText = 'No Result Found';
+        return;
+    }
+
+    //if data is suggestion 
+    if (typeof data[0] === 'string') {
+        loading.style.display = 'none';
+        let heading = document.createElement('h3');
+        heading.innerText = "Did you mean?"
+        notFound.appendChild(heading);
+
+        data.forEach(element => {
+            let suggestion = document.createElement('span');
+            suggestion.classList.add('suggested');
+            suggestion.innerText = element;
+            notFound.appendChild(suggestion);
+        })
+        return;
+    }
+
+    //Result Found
+    loading.style.display = 'none';
+    let definition = data[0].shortdef[0];
+
+    defBox.innerText = definition;
+
+    //sound 
+    const soundName = data[0].hwi.prs[0].sound.audio;
+    if (soundName) {
+        //sound is available
+        renderSound(soundName);
+    }
+}
+
+function renderSound(soundName) {
+    //https://media.merriam-webster.com/soundc11
+    let subfolder = soundName.charAt(0);
+
+    let soundSrc = `https://media.merriam-webster.com/soundc11/${subfolder}/${soundName}.wav?key=${apiKey}`;
+
+    let audio = document.createElement("audio");
+    audio.src = soundSrc;
+    audio.controls = true;
+
+    audioBox.appendChild(audio);
+
+}
