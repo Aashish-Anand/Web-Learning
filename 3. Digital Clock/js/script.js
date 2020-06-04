3ca649c5472c50dc6b0ecac803f758513964df59
Aashish-Anand <aashishanand1997@gmail.com>
2020-06-05 03:50:00 +0530

    created a digital clock

---
+++ 3. Digital Clock/js/script.js
@@ -0,0 +1,29 @@
+let clock = document.getElementById("clock");
+let meridian = "AM";
+
+function getTime() {
+    let date = new Date();
+
+    var hours = date.getHours() % 12;
+    var minutes = date.getMinutes();
+    var seconds = date.getSeconds();
+    if (hours > 12) {
+        meridian = "PM";
+    }
+
+    if (hours < 10) {
+        hours = "0" + hours;
+    }
+
+    if (minutes < 10) {
+        minutes = "0" + minutes;
+    }
+    if (seconds < 10) {
+        seconds = "0" + seconds;
+    }
+
+    let time = hours + ":" + minutes + ":" + seconds + " " + meridian + "!";
+    clock.innerText = time;
+}
+
+setInterval(getTime, 1000);
